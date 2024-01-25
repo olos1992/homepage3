6b4ce7388df2f5a81fee28193abf0597a5cbfdeb
Aleksander Skuła <aleksander.skula@gmail.com>
2024-01-25 20:42:00 +0100

    Module 3 changes added

--- js/script.js
+++ js/script.js
@@ -6,9 +6,11 @@ let deleteButton = document.querySelector(".deleteButton");
 let backgroundButton = document.querySelector(".backgroundButton");
 
 deleteButton.addEventListener("click", () => {
-    image.remove();
+    image.classList.toggle("imageDeleted");
+    image.classList.contains("imageDeleted") ? deleteButton.innerText = "Przywróć zdjęcie" : deleteButton.innerText = "Usuń zdjęcie";
 });
 
 backgroundButton.addEventListener("click", () => {
     body.classList.toggle("background");
+    body.classList.contains("background") ? backgroundButton.innerText = "Przywróć tło" : backgroundButton.innerText = "Zmień tło";
 });
