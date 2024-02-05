6144b1a0ec14f68f1daded16ebda00b842430adf
Aleksander Skula <aleksander.skula@gmail.com>
2024-02-05 18:49:45 +0100

    Webpage refactoring using BEM convention

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-console.log("Cześć");
+console.log("Witaj, deweloperze");
 
-let image = document.querySelector(".image");
+let image = document.querySelector(".js-profileImage");
 let body = document.querySelector("body");
-let deleteButton = document.querySelector(".deleteButton");
-let backgroundButton = document.querySelector(".backgroundButton");
+let deleteButton = document.querySelector(".js-deletePhotoButton");
+let backgroundButton = document.querySelector(".js-changeBackgroundButton");
 
 deleteButton.addEventListener("click", () => {
-    image.classList.toggle("imageDeleted");
-    image.classList.contains("imageDeleted") ? deleteButton.innerText = "Przywróć zdjęcie" : deleteButton.innerText = "Usuń zdjęcie";
+    image.classList.toggle("main__image--hidden");
+    image.classList.contains("main__image--hidden") ? deleteButton.innerText = "Przywróć zdjęcie" : deleteButton.innerText = "Usuń zdjęcie";
 });
 
 backgroundButton.addEventListener("click", () => {
-    body.classList.toggle("background");
-    body.classList.contains("background") ? backgroundButton.innerText = "Przywróć tło" : backgroundButton.innerText = "Zmień tło";
+    body.classList.toggle("body--dark");
+    body.classList.contains("body--dark") ? backgroundButton.innerText = "Przywróć tło" : backgroundButton.innerText = "Zmień tło";
 });
